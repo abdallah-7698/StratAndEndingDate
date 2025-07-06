d1e72eac9acfdeb081285d4a8fb6d6875535b661
abdallah.mohammedabdallah.mohammedabdallah.mohammed <abdallah.moha.github.comabdallah-moha@gmail.com>
2025-07-07 00:46:36 +0300

    Initial Commit

---
+++ StratAndEndingDate/StratAndEndingDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StratAndEndingDateApp.swift
+//  StratAndEndingDate
+//
+//  Created by name on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct StratAndEndingDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
